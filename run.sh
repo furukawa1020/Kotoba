a5477b9003cafd7348609144dd200246c6b0b46c
furukawa　kotaro <f.kotaro.0530@gmail.com>
2026-01-02 08:58:30 +0900

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-cd "$(dirname "$0")" || exit 1
-echo "=== Kotoba Runtime ==="
-echo "Time: $(date)" > logs/last.log
-./kotoba work/current.kb >> logs/last.log 2>&1
-cat logs/last.log
